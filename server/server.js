88d1628f715ccf78be7d4adbff740691eb43d303
icrustandi <icrustandi@gmail.com>
2017-09-05 16:03:07 -0700

    Express server to serve index.html

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.static(__dirname +'./../')); //serves the index.html
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(3000);
