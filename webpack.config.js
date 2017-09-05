8e64632d6624f85ad6a6606bd937a77f9f6e60b3
icrustandi <icrustandi@gmail.com>
2017-09-05 15:55:47 -0700

    Initialized Skeleton, added dependencies

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+module.exports = {
+  entry: ['./client/index.js'],
+  output: {
+    path: path.resolve('./bundle'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+}
