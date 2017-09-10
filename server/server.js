8dbb697da9cf973ae90bcfdd5c254b1a40b392c8
pennwu1 <phw.dental@gmail.com>
2017-09-09 17:48:46 -0700

    remove GET route for debugging

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname +'./../')); //serves the index.html
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.listen(port, () => {
-//   console.log('App listening on port ' + port)
-// });
+app.listen(port, () => {
+  console.log('App listening on port ' + port);
+});
 
-module.exports.app = app;
+module.exports = app;
