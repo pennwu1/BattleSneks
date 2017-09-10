f9c1d7acaedcfda5c33a3ae4c9699db5d60dcb35
pennwu1 <phw.dental@gmail.com>
2017-09-09 17:49:39 -0700

    update server integration tests

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,62 +1,103 @@
-import { app } from '../server/server.js';
-const port = process.envPORT || 3000;
+let app = require('../server/server.js')
+const testUser = {
+  username: 'test',
+  password: '1234'
+}
+const testUser2 = {
+  username: 'test2',
+  password: '2345'
+}
 
 describe('loading Express', () => {
-  let server;
-  before(() => {
-    server = app.listen(port);
-    console.log('server', server);
-  });
-  after(() => {
-    server.close();
-  })
   it('responds to / with a Content Type Header', (done) => {
-    request(server)
-    .get('/')
-    .expect(400)
+    request(app)
+    .get(`/`)
+    .expect(200)
     .expect('Content-Type', /html/)
     .end((err, res) => {
+      if (err) throw err;
       done();
     });
   });
 
-  describe('Sign up has valid username and password', () => {
-    it('Returns 404 when username is missing', (done) => {
-      request(server)
+  describe('Signup', () => {
+    it('Returns 400 when username is missing', (done) => {
+      request(app)
       .post('/signup')
-      .send({
-        password: 1234
-      })
-      .expect(200)
+      .send({ password: '1234' })
+      .expect(400)
       .end((err, res) => {
         if (err) throw err
         done();
-      })
+      });
     });
-    it('Returns 404 when password is missing', (done) => {
-      request(server)
+    it('Returns 400 when password is missing', (done) => {
+      request(app)
       .post('/signup')
-      .send({
-        username: 'test'
-      })
-      .expect(200)
+      .send({ username: 'test' })
+      .expect(400)
       .end((err, res) => {
         if (err) throw err
         done();
-      })
-    })
-    it('Returns 404 when new user is created', (done) => {
-      request(server)
-      .post('/signup')
-      .send({
-        username: 'Test',
-        password: 1234
-      })
+      });
+    });
+    describe('Signup with username and password', () => {
+      it('Returns 200 when new user is successfully created', (done) => {
+        request(app)
+        .post('/signup')
+        .send(testUser)
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err
+          done();
+        });
+      });
+      it('Returns 401 when user has already already created', (done) => {
+        request(app)
+        .post('/signup')
+        .send(testUser)
+        .expect(401)
+        .end((err, res) => {
+          if (err) throw err
+          done();
+        });
+      });
+    });
+  });
+  describe('Sign in', () => {
+    it('Returns 200 when existing user signs in', (done) => {
+      request(app)
+      .get('/signin')
+      .send(testUser)
       .expect(200)
       .end((err, res) => {
         if (err) throw err
         done();
       });
     });
+    describe('Sign in fails', () => {
+      it('Returns 401 when a non-existing user signs in', (done) => {
+        request(app)
+        .get('/signin')
+        .send(testUser2)
+        .expect(401)
+        .end((err, res) => {
+          if (err) throw err
+          done();
+        });
+      });
+      it('Redirects to signup page when non-existing user signs in', (done) => {
+        request(app)
+        .get('/signin')
+        .send(testUser2)
+        .expect(401)
+        .end((err, res) => {
+          if (!res.header.location) throw err;
+          expect(res.header['location']).contains('/home')
+          done();
+        });
+      });
+    });
   });
 });
+
