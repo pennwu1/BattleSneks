587832edfe60e1d60c05f19071a20b1e4ebe241c
pennwu1 <phw.dental@gmail.com>
2017-09-11 22:46:27 -0700

    change HTTP error messages and test description names

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,15 +1,8 @@
 let app = require('../server/server.js')
-const testUser = {
-  username: 'test',
-  password: '1234'
-}
-const testUser2 = {
-  username: 'test2',
-  password: '2345'
-}
+const [testUser, testUser2] = [{username: 'test', password: '1234'}, { username:'testUser2', password:'2345'}];
 
-describe('loading Express', () => {
-  it('responds to / with a Content Type Header', (done) => {
+describe('Loading Express', () => {
+  it('responds to / with a HTML Content Type Header', (done) => {
     request(app)
     .get(`/`)
     .expect(200)
@@ -19,85 +12,81 @@ describe('loading Express', () => {
       done();
     });
   });
+});
 
-  describe('Signup', () => {
-    it('Returns 400 when username is missing', (done) => {
-      request(app)
-      .post('/signup')
-      .send({ password: '1234' })
-      .expect(400)
-      .end((err, res) => {
-        if (err) throw err
-        done();
-      });
-    });
-    it('Returns 400 when password is missing', (done) => {
-      request(app)
-      .post('/signup')
-      .send({ username: 'test' })
-      .expect(400)
-      .end((err, res) => {
-        if (err) throw err
-        done();
-      });
-    });
-    describe('Signup with username and password', () => {
-      it('Returns 200 when new user is successfully created', (done) => {
-        request(app)
-        .post('/signup')
-        .send(testUser)
-        .expect(200)
-        .end((err, res) => {
-          if (err) throw err
-          done();
-        });
-      });
-      it('Returns 401 when user has already already created', (done) => {
-        request(app)
-        .post('/signup')
-        .send(testUser)
-        .expect(401)
-        .end((err, res) => {
-          if (err) throw err
-          done();
-        });
-      });
+describe('Sign up', () => {
+  it('Returns error when username is missing', (done) => {
+    request(app)
+    .post('/signup')
+    .send({ password: '1234' })
+    .expect(400)
+    .end((err, res) => {
+      if (err) throw err
+      done();
     });
   });
-  describe('Sign in', () => {
-    it('Returns 200 when existing user signs in', (done) => {
-      request(app)
-      .get('/signin')
-      .send(testUser)
-      .expect(200)
-      .end((err, res) => {
-        if (err) throw err
-        done();
-      });
+  it('Returns error when password is missing', (done) => {
+    request(app)
+    .post('/signup')
+    .send({ username: 'test' })
+    .expect(400)
+    .end((err, res) => {
+      if (err) throw err
+      done();
     });
-    describe('Sign in fails', () => {
-      it('Returns 401 when a non-existing user signs in', (done) => {
-        request(app)
-        .get('/signin')
-        .send(testUser2)
-        .expect(401)
-        .end((err, res) => {
-          if (err) throw err
-          done();
-        });
-      });
-      it('Redirects to signup page when non-existing user signs in', (done) => {
-        request(app)
-        .get('/signin')
-        .send(testUser2)
-        .expect(401)
-        .end((err, res) => {
-          if (!res.header.location) throw err;
-          expect(res.header['location']).contains('/home')
-          done();
-        });
-      });
+  });
+  it('New user is successfully created', (done) => {
+    request(app)
+    .post('/signup')
+    .send(testUser)
+    .expect(200)
+    .end((err, res) => {
+      if (err) throw err
+      done();
+    });
+  });
+  it('User has already been created', (done) => {
+    request(app)
+    .post('/signup')
+    .send(testUser)
+    .expect(400)
+    .end((err, res) => {
+      if (err) throw err
+      done();
     });
   });
 });
-
+describe('Sign in', () => {
+  it('User successfully logged in', (done) => {
+    request(app)
+    .get('/signin')
+    .send(testUser)
+    .expect(302)
+    .end((err, res) => {
+      if (err) throw err
+      expect(res.header['location']).contains('/home');
+      done();
+    });
+  });
+  it('Non-existing user signed in', (done) => {
+    request(app)
+    .get('/signin')
+    .send(testUser2)
+    .expect(401)
+    .end((err, res) => {
+      expect(res.header['location']).contains('/signup');
+      done();
+    });
+  });
+  // it('Redirects to home page when non-existing user signs in', (done) => {
+  //   request(app)
+  //   .get('/signin')
+  //   .send(testUser2)
+  //   .expect(401)
+  //   .end((err, res) => {
+  //     if (!res.header.location) throw err;
+  //     expect(res.header['location']).contains('/home');
+  //     done();
+  //   });
+  // });
+});
