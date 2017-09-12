3bb240234356fa8a50b006cb2ce653082e5043e6
pennwu1 <phw.dental@gmail.com>
2017-09-11 22:44:58 -0700

    add signup and signin routes

--- server/server.js
+++ server/server.js
@@ -3,10 +3,18 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const userController = require('../database/users/userController');
 
 app.use(express.static(__dirname +'./../')); //serves the index.html
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Sign up logic
+app.post('/signup', userController.createUser); // redirect to home page
+app.get('/signin', userController.findUser); // redirect to home page
+// app.get('/home', ((req, res) => { console.log('hi') }));
 
 app.listen(port, () => {
   console.log('App listening on port ' + port);
